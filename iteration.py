f961f1ba15fe6ac710943dd9047ed85d7ccffcae
PrajwalDeula <69007189+PrajwalDeula@users.noreply.github.com>
2023-10-22 19:24:13 +0545

    Add files via upload
    Iternation using __iter__ and__next__ function.  Use the iter() method and list the 1 to 5.

---
+++ iteration.py
@@ -0,0 +1,18 @@
+class myRun:
+    def __iter__(self):
+        self.a = 1
+        return self
+    
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+myClass = myRun()
+myIter = iter(myClass)
+
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
+print(next(myIter))
